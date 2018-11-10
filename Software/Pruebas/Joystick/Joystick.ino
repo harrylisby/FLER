2383d21de9dd8f21f1d0aded9f3a3ea88f1642f5
AlisonLis <39997150+AlisonLis@users.noreply.github.com>
2018-11-10 15:13:46 -0600

    Joystick con ADC y WEMOS

--- Software/Pruebas/Joystick/Joystick.ino
+++ Software/Pruebas/Joystick/Joystick.ino
@@ -1,30 +1,26 @@
-//Joystick derecho
-const int ejeY = PB0;
-const int ejeX = PB1;
-//Joystick derecho con boton
-const int boton = PB10;
-//Joystick izquierdo
-const int ejeI = PA6;
-const int ejeR = PA7;
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
 
+Adafruit_ADS1115 ads(0x48);
+
+int16_t adc2;
+int16_t adc3;
+int16_t adcZ;
+int16_t adcG;
+
+//Joystick derecho con boton
+const int boton = 0;
 int value_boton;
-int value_ejeY;
-int value_ejeX;
-int value_ejeZ;
-int value_ejeG;
-int value_ejeI;
-int value_ejeR;
 
 uint32_t prevMillis;
 uint32_t actMillis;
 
 void setup() {
-  pinMode(ejeY, INPUT_ANALOG);
-  pinMode(ejeX, INPUT_ANALOG);
+  Wire.begin();
+ 	Serial.begin(115200); // Ignored by Maple. But needed by boards using Hardware serial via a USB to Serial Adaptor
+  ads.begin();
+  ads.setGain(GAIN_ONE);
   pinMode(boton, INPUT_PULLUP);
-  pinMode(ejeI, INPUT_ANALOG);
-  pinMode(ejeR, INPUT_ANALOG);
-  	Serial.begin(115200); // Ignored by Maple. But needed by boards using Hardware serial via a USB to Serial Adaptor
 }
 
 void loop() {
@@ -34,40 +30,42 @@ void loop() {
     //su codigo aqui
     // Read the analog input into a variable:
     value_boton = digitalRead(boton);
-    value_ejeI = analogRead(ejeI);
-    value_ejeR = analogRead(ejeR);
-    // print the result:
-
+    //Se lee el valor del eje I y R
+    int16_t adc0=ads.readADC_SingleEnded(0);
+    int16_t adc1=ads.readADC_SingleEnded(1);
+    
     if(value_boton == 1){
-      value_ejeY = analogRead(ejeY);
-      value_ejeX = analogRead(ejeX);
-      value_ejeZ = 2047;
-      value_ejeG = 2047;
+      //Leer eje Y y X
+      adc2=ads.readADC_SingleEnded(2);
+      adc3=ads.readADC_SingleEnded(3);
+      adcZ = 13150;
+      adcG = 13150;
     }
     else{
-      value_ejeZ = analogRead(ejeY);
-      value_ejeG = analogRead(ejeX);
-      value_ejeY = 2047;
-      value_ejeX = 2047;
+      //Leer eje Z y G
+      adcZ=ads.readADC_SingleEnded(2);
+      adcG=ads.readADC_SingleEnded(3);
+      adc2 = 13150;
+      adc3 = 13150;
     }
 
     Serial.print("X: ");
-    Serial.print(value_ejeX);
+    Serial.print(adc3);
     Serial.print(" ");
     Serial.print("Y: ");
-    Serial.print(value_ejeY);
+    Serial.print(adc2);
     Serial.print(" ");
     Serial.print("Z: ");
-    Serial.print(value_ejeZ);
+    Serial.print(adcZ);
     Serial.print(" ");
     Serial.print("G: ");
-    Serial.print(value_ejeG);
+    Serial.print(adcG);
     Serial.print(" ");
     Serial.print("I: ");
-    Serial.print(value_ejeI);
+    Serial.print(adc0);
     Serial.print(" ");
     Serial.print("R: ");
-    Serial.print(value_ejeR);
+    Serial.print(adc1);
     Serial.println();
   }
 }
