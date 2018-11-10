fea9a654fd3c62e1b4907e3854829a7e1c623875
Knnthlv <knnthlv07@gmail.com>
2018-11-10 10:57:42 -0600

    actualización de estructuras
    se agregan estructuras para que coincidan con el código TX

--- Software/Pruebas Kenneth/EasyTransfer_RX_Test/EasyTransfer_RX_Test.ino/EasyTransfer_RX_Test.ino.ino
+++ Software/Pruebas Kenneth/EasyTransfer_RX_Test/EasyTransfer_RX_Test.ino/EasyTransfer_RX_Test.ino.ino
@@ -16,47 +16,46 @@
 
 int16_t SP_IF1, SP_IF2, SP_IF3, SP_DF1, SP_DF2, SP_DF3, SP_IR1, SP_IR2, SP_IR3, SP_DR1, SP_DR2, SP_DR3;
 
-
-
 //create object
-EasyTransfer ET; 
+EasyTransfer Front;
+EasyTransfer Rear; 
 
-struct RECEIVE_DATA_STRUCTURE{
-  //put your variable definitions here for the data you want to receive
-  //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
-  int16_t blinks;
-  int16_t pause;
+//*********************************************************************
+struct SEND_DATA_STRUCTURE{
+
+  int16_t SSP_IF1, SSP_IF2, SSP_IF3, SSP_DF1, SSP_DF2, SSP_DF3;
+  
 };
 
 //give a name to the group of data
-RECEIVE_DATA_STRUCTURE mydata;
+SEND_DATA_STRUCTURE frontData;
+//*********************************************************************
 
+
+//*********************************************************************
+struct SEND_DATA_STRUCTURE_2{
+
+  int16_t SSP_IR1, SSP_IR2, SSP_IR3, SSP_DR1, SSP_DR2, SSP_DR3;
+  
+};
+
+SEND_DATA_STRUCTURE_2 rearData;
+//*********************************************************************
 void setup(){
   Serial.begin(115200);
   Serial2.begin(9600);
-  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc. 
-  ET.begin(details(mydata), &Serial2);
+  Serial3.begin(9600);
   
-  pinMode(PC13, OUTPUT);
+  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc. 
+  Front.begin(details(frontData), &Serial2);
+  Rear.begin(details(rearData), &Serial3);
   
 }
 
 void loop(){
-  //check and see if a data packet has come in. 
-  if(ET.receiveData()){
+  //send the data
 
-    Serial.println("B: " + String(mydata.blinks) + "P: " + String(mydata.pause));
-    
-    //this is how you access the variables. [name of the group].[variable name]
-    //since we have data, we will blink it out. 
-    for(int i = mydata.blinks; i>0; i--){
-      digitalWrite(PC13, HIGH);
-      delay(mydata.pause * 100);
-      digitalWrite(PC13, LOW);
-      delay(mydata.pause * 100);
-    }
-  }
+  Front.sendData();
+  Rear.sendData();
   
-  //you should make this delay shorter then your transmit delay or else messages could be lost
-  delay(250);
 }
