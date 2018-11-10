5adb1bdd053a3b7b89730a15fcf8bc3cb51b759e
Knnthlv <knnthlv07@gmail.com>
2018-11-10 12:24:40 -0600

    acomodo de estructuras

--- Software/Pruebas Kenneth/EasyTransfer_RX_Test/EasyTransfer_RX_Test.ino/EasyTransfer_RX_Test.ino.ino
+++ Software/Pruebas Kenneth/EasyTransfer_RX_Test/EasyTransfer_RX_Test.ino/EasyTransfer_RX_Test.ino.ino
@@ -1,7 +1,8 @@
 #include <EasyTransfer.h>
 
 
-int16_t SP_IF1, SP_IF2, SP_IF3, SP_DF1, SP_DF2, SP_DF3, SP_IR1, SP_IR2, SP_IR3, SP_DR1, SP_DR2, SP_DR3;
+int16_t SP_IF1, SP_IF2, SP_IF3, SP_DF1, SP_DF2, SP_DF3; //Variables frontales
+int16_t SP_IR1, SP_IR2, SP_IR3, SP_DR1, SP_DR2, SP_DR3; //Variables traseras
 
 //create object
 EasyTransfer Front;
@@ -34,22 +35,15 @@ void setup(){
   Serial3.begin(9600);
   
   //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc. 
-  Front.begin(details(frontData), &Serial2);
-  Rear.begin(details(rearData), &Serial3);
+  Front.begin(details(frontData), &Serial2); //comunicación frontal
+  Rear.begin(details(rearData), &Serial3); //comunicación trasera
   
 }
 
 void loop(){
   //send the data
   Front.receiveData();
-  Rear.receiveData();
-  
-  serialDecoder ();
-}
-
-
-void serialDecoder (){
-   
+     
   SP_IF1 = frontData.SSP_IF1;
   SP_IF2 = frontData.SSP_IF2;
   SP_IF3 = frontData.SSP_IF3;
@@ -57,7 +51,18 @@ void serialDecoder (){
   SP_DF2 = frontData.SSP_DF2;
   SP_DF3 = frontData.SSP_DF3;
         
-  Serial.println("SP_IF1: " + String(SP_IF1)+ " SP_IF2: " + String(SP_IF2)+ " SP_IF3: " + String(SP_IF3) + " SP_IR1: " + String(SP_IR1)+ " SP_IR2: " + String(SP_IR2)+ " SP_IR3: " + String(SP_IR3));   
-      }
+  /*Serial.println("SP_IF1: " + String(SP_IF1)+ " SP_IF2: " + String(SP_IF2)+ " SP_IF3: " + String(SP_IF3) + " SP_DF1: " + String(SP_DF1)+ " SP_DF2: " + String(SP_DF2)+ " SP_DF3: " + String(SP_DF3));*/   
+
+  Rear.receiveData();
+
+  SP_IR1 = rearData.SSP_IR1;
+  SP_IR2 = rearData.SSP_IR2;
+  SP_IR3 = rearData.SSP_IR3;
+  SP_DR1 = rearData.SSP_DR1;
+  SP_DR2 = rearData.SSP_DR2;
+  SP_DR3 = rearData.SSP_DR3;
+
+  /*Serial.println("SP_IR1: " + String(SP_IR1)+ " SP_IR2: " + String(SP_IR2)+ " SP_IR3: " + String(SP_IR3) + " SP_DR1: " + String(SP_DR1)+ " SP_DR2: " + String(SP_DR2)+ " SP_DR3: " + String(SP_DR3));*/ 
+}
  
 
