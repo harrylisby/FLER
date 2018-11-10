b249fa69b08a2914ee86780d2ffedc6289ffdc9f
Knnthlv <knnthlv07@gmail.com>
2018-11-10 13:18:11 -0600

    Definición de posiciones
    Se crean las definiciones para adelante y atrás

--- Software/Pruebas Kenneth/EasyTransfer_RX_Test/EasyTransfer_RX_Test.ino/EasyTransfer_RX_Test.ino.ino
+++ Software/Pruebas Kenneth/EasyTransfer_RX_Test/EasyTransfer_RX_Test.ino/EasyTransfer_RX_Test.ino.ino
@@ -1,5 +1,6 @@
 #include <EasyTransfer.h>
 
+#define FRONT
 
 int16_t SP_IF1, SP_IF2, SP_IF3, SP_DF1, SP_DF2, SP_DF3; //Variables frontales
 int16_t SP_IR1, SP_IR2, SP_IR3, SP_DR1, SP_DR2, SP_DR3; //Variables traseras
@@ -8,6 +9,7 @@ int16_t SP_IR1, SP_IR2, SP_IR3, SP_DR1, SP_DR2, SP_DR3; //Variables traseras
 EasyTransfer Front;
 EasyTransfer Rear; 
 
+#ifdef FRONT
 //*********************************************************************
 struct RECEIVED_DATA_STRUCTURE{
 
@@ -18,8 +20,9 @@ struct RECEIVED_DATA_STRUCTURE{
 //give a name to the group of data
 RECEIVED_DATA_STRUCTURE frontData;
 //*********************************************************************
+#endif
 
-
+#ifdef REAR
 //*********************************************************************
 struct RECEIVED_DATA_STRUCTURE_2{
 
@@ -29,18 +32,25 @@ struct RECEIVED_DATA_STRUCTURE_2{
 
 RECEIVED_DATA_STRUCTURE_2 rearData;
 //*********************************************************************
+#endif
+
 void setup(){
   Serial.begin(115200);
   Serial2.begin(9600);
   Serial3.begin(9600);
   
   //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc. 
+#ifdef FRONT  
   Front.begin(details(frontData), &Serial2); //comunicación frontal
-  Rear.begin(details(rearData), &Serial3); //comunicación trasera
-  
+#endif
+#ifdef REAR
+  Rear.begin(details(rearData), &Serial2); //comunicación trasera
+#endif
 }
 
 void loop(){
+
+#ifdef FRONT
   //send the data
   Front.receiveData();
      
@@ -52,6 +62,9 @@ void loop(){
   SP_DF3 = frontData.SSP_DF3;
         
   /*Serial.println("SP_IF1: " + String(SP_IF1)+ " SP_IF2: " + String(SP_IF2)+ " SP_IF3: " + String(SP_IF3) + " SP_DF1: " + String(SP_DF1)+ " SP_DF2: " + String(SP_DF2)+ " SP_DF3: " + String(SP_DF3));*/   
+#endif
+
+#ifdef REAR
 
   Rear.receiveData();
 
@@ -63,6 +76,7 @@ void loop(){
   SP_DR3 = rearData.SSP_DR3;
 
   /*Serial.println("SP_IR1: " + String(SP_IR1)+ " SP_IR2: " + String(SP_IR2)+ " SP_IR3: " + String(SP_IR3) + " SP_DR1: " + String(SP_DR1)+ " SP_DR2: " + String(SP_DR2)+ " SP_DR3: " + String(SP_DR3));*/ 
+#endif
 }
  
 
