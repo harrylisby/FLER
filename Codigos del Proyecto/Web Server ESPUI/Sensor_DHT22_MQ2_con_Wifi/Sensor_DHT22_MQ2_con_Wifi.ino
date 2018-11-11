c299176cb129836cff327367b55903793193d0b7
harrylisby <harry@lisby.co>
2018-11-10 23:16:52 -0600

    reparación de código

--- Codigos del Proyecto/Web Server ESPUI/Sensor_DHT22_MQ2_con_Wifi/Sensor_DHT22_MQ2_con_Wifi.ino
+++ Codigos del Proyecto/Web Server ESPUI/Sensor_DHT22_MQ2_con_Wifi/Sensor_DHT22_MQ2_con_Wifi.ino
@@ -2,78 +2,118 @@
 #include <DHT_U.h>
 #include "Wire.h"
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
 #include <ESPUI.h>
 
+//Configuración WiFi
 const char ssid[] = "FLER";
 const char pass[] = "fler12345";
 
-
-ESP8266WebServer server(80); 
+//Almacenamiento de sensores
 float hums;
 float temps;
 float gass;
+
+//Configuración de sensores
 #define DHTTYPE DHT22
 #define DHTPIN  D4
 DHT dht(DHTPIN, DHTTYPE);
-
 int INPUT_PIN = A0;
 
+//Delay
+int timeTracker = 0; //Tiempo transcurrido
+int timeInterval = 1000; //Milisegundos entre cada ejecución de programa
 
-void handleRoot() {
-  server.send(200, "text/plain", "hello from esp8266!");
+
+void padExample(Control sender, int value) {
+  switch (value) {
+  case P_LEFT_DOWN:
+    Serial.print("left down");
+    break;
+  case P_LEFT_UP:
+    Serial.print("left up");
+    break;
+  case P_RIGHT_DOWN:
+    Serial.print("right down");
+    break;
+  case P_RIGHT_UP:
+    Serial.print("right up");
+    break;
+  case P_FOR_DOWN:
+    Serial.print("for down");
+    break;
+  case P_FOR_UP:
+    Serial.print("for up");
+    break;
+  case P_BACK_DOWN:
+    Serial.print("back down");
+    break;
+  case P_BACK_UP:
+    Serial.print("back up");
+    break;
+  case P_CENTER_DOWN:
+    Serial.print("center down");
+    break;
+  case P_CENTER_UP:
+    Serial.print("center up");
+    break;
+  }
+  Serial.print(" ");
+  Serial.println(sender.id);
 }
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("");
-  
-  
+void setup(){
+
+  //Iniciar serial y sensores
+  Serial.begin(115200);
   pinMode(INPUT_PIN, INPUT);
   Serial.setTimeout(2000);
   dht.begin(); 
   Wire.begin();
 
-
+  //Inicialización de access point
   WiFi.mode(WIFI_AP);
   WiFi.hostname(ssid);
-  //WiFi.softAP(ssid);
   WiFi.softAP(ssid, pass);
   Serial.println("");
   Serial.print("IP address: ");
   Serial.println(WiFi.softAPIP());
 
-                 
-  ESPUI.label("Humedad:", COLOR_TURQUOISE, String(hums));
-  ESPUI.label("Temperatura:", COLOR_EMERALD, String(temps) );
-  ESPUI.label("Saturación:", COLOR_WETASPHALT, String(gass) );
+  //Inicialización de bloques de interfaz
+  ESPUI.label("Humedad:", COLOR_WETASPHALT, "0");
+  ESPUI.label("Temperatura:", COLOR_WETASPHALT, "0");
+  ESPUI.label("Saturación:", COLOR_WETASPHALT, "0");
+  ESPUI.pad("Controlador", true, &padExample, COLOR_WETASPHALT);
+  //ESPUI.pad("Controlador2", false, &padExample, COLOR_WETASPHALT);
+  
   ESPUI.begin("ESPUI Control");
-  }
+}
 
 
 void loop(){ 
+  int currentTime=millis();  //Ejecuta el código sin uso de delay
+  if((currentTime-timeTracker)>timeInterval){
+    timeTracker=currentTime;
 
-  server.handleClient(); 
-  
-  hums = dht.readHumidity();
-  temps = dht.readTemperature();
-  gass = analogRead(INPUT_PIN);
-  Serial.print("Humidity: ");
-  Serial.print(hums, 2);
-  Serial.print(" Temperature: ");
-  Serial.print(temps, 2);
-  Serial.print(" Gas: ");
-  Serial.print(gass, 2);
-  Serial.println();
-//  client.connect(server, 80);
-//  client.println (gass,2);
-//  client.flush();
-//  delay(100);   
-//  client.println(hums,2);
-//  client.flush();
-//  delay(100);
-//  client.println(temps,2);
-//  client.flush();
- delay(1000);
+    //Inicio del programa
+    //Lectura de sensores
+    hums = dht.readHumidity();
+    temps = dht.readTemperature();
+    gass = analogRead(INPUT_PIN);
+
+    //Impresión a monitor serial
+    Serial.print("Humidity: ");
+    Serial.print(hums, 2);
+    Serial.print(" Temperature: ");
+    Serial.print(temps, 2);
+    Serial.print(" Gas: ");
+    Serial.print(gass, 2);
+    Serial.println();
+
+    //Impresión a interface web
+    ESPUI.print("Humedad:", String(hums));
+    ESPUI.print("Temperatura:", String(temps));
+    ESPUI.print("Saturación:", String(gass));
+
+    
+  }
 }
