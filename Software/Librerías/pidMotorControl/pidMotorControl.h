dde4b24a23b7e44e72400022d88f4b39d4e53f1b
harrylisby <harrylisby@users.noreply.github.com>
2018-11-14 16:05:54 -0600

    Intentos de librería

--- Software/Librerías/pidMotorControl/pidMotorControl.h
+++ Software/Librerías/pidMotorControl/pidMotorControl.h
@@ -21,3 +21,50 @@ private:
 	
 };
 #endif
+
+
+
+
+
+
+class Customer {
+public:
+    Customer(void);
+    Customer(string,char,int);
+};
+
+
+
+
+
+
+
+#include "Customer.h"
+
+class Node {
+   public:
+
+      Node(void); //default constructor
+      Node(string,char,int); //Node constructor with customer details
+
+      Node* left;
+      Node* right;
+      Node* parent;
+      Customer customer;
+};
+
+
+
+
+
+
+
+Node::Node() : left(nullptr),
+               right(nullptr),
+               parent(nullptr),
+               customer() {}
+
+Node::Node(string x, char y, int z) : left(nullptr),
+                                      right(nullptr),
+                                      parent(nullptr),
+                                      customer(x, y, z) {}
