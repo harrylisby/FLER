ad6c32d83aa37a2918aa89cb4eb0c2ef697c9b4b
AlisonLis <39997150+AlisonLis@users.noreply.github.com>
2018-11-17 20:06:36 -0600

    Update Calculo_Angulos.ino
    Incluye el cálculo de todos los ángulos.

--- Software/Pruebas/Calculo_Angulos/Calculo_Angulos/Calculo_Angulos.ino
+++ Software/Pruebas/Calculo_Angulos/Calculo_Angulos/Calculo_Angulos.ino
@@ -10,6 +10,13 @@ double angulo2A;
 double c2B = 165;
 double angulo2B;
 
+double c3A;
+double c3B;
+double c4A =200; //elegir variable (prueba)
+double angulo4A;
+double c4B =200; //elegir variable (prueba)
+double angulo4B;
+
 void setup() {
   Serial.begin(115200);
 }
@@ -19,6 +26,10 @@ void loop() {
   angulo1B = acos(c1B/p1);
   angulo2A = acos(c2A/p2);
   angulo2B = acos(c2B/p2);
+  c3A = c1A + c2A;
+  c3B = c1B + c2B;
+  angulo4A = acos(c4A/c3A);
+  angulo4B = acos(c4B/c3B);
 
   Serial.print("Ángulo 1A: ");
   Serial.print(angulo1A);
@@ -27,5 +38,9 @@ void loop() {
   Serial.print("  Ángulo 2A: ");
   Serial.print(angulo2A);
   Serial.print("  Ángulo 2B: ");
-  Serial.println(angulo2B);
+  Serial.print(angulo2B);
+  Serial.print("  Ángulo 4A: ");
+  Serial.print(angulo4A);
+  Serial.print("  Ángulo 4B: ");
+  Serial.println(angulo4B);
 }
