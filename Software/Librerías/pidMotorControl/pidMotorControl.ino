08619b674769639846aeccbda379642cf729a20e
harrylisby <harry@lisby.co>
2018-11-21 18:50:50 -0600

    comunicación completa

--- Software/Librerías/pidMotorControl/pidMotorControl.ino
+++ Software/Librerías/pidMotorControl/pidMotorControl.ino
@@ -1,4 +1,40 @@
 #include "pidMotorControl.h" 
+#include <EasyTransfer.h>
+
+#define FRONT
+
+int16_t SP_IF1, SP_IF2, SP_IF3, SP_DF1, SP_DF2, SP_DF3; //Variables frontales
+int16_t SP_IR1, SP_IR2, SP_IR3, SP_DR1, SP_DR2, SP_DR3; //Variables traseras
+
+
+#ifdef FRONT
+//*********************************************************************
+EasyTransfer Front;
+
+struct RECEIVED_DATA_STRUCTURE{
+
+  int16_t SSP_IF1, SSP_IF2, SSP_IF3, SSP_DF1, SSP_DF2, SSP_DF3;
+  
+};
+
+//give a name to the group of data
+RECEIVED_DATA_STRUCTURE frontData;
+//*********************************************************************
+#endif
+
+#ifdef REAR
+//*********************************************************************
+EasyTransfer Rear; 
+
+struct RECEIVED_DATA_STRUCTURE_2{
+
+  int16_t SSP_IR1, SSP_IR2, SSP_IR3, SSP_DR1, SSP_DR2, SSP_DR3;
+  
+};
+
+RECEIVED_DATA_STRUCTURE_2 rearData;
+//*********************************************************************
+#endif
 
 //IF1 IO
 static int IF1_ENC = PA0;
@@ -47,35 +83,71 @@ double IF1_KD=175, IF2_KD=175, IF3_KD=175, DF1_KD=175, DF2_KD=175, DF3_KD=175;
 pidControl IF1(&IF1_ENCREAD,&IF1_OUT,&IF1_SP,IF1_KP,IF1_KD,IF1_KI);
 pidControl IF2(&IF2_ENCREAD,&IF2_OUT,&IF2_SP,IF2_KP,IF2_KD,IF2_KI);
 pidControl IF3(&IF3_ENCREAD,&IF3_OUT,&IF3_SP,IF3_KP,IF3_KD,IF3_KI);
-pidControl DF2(&DF1_ENCREAD,&DF1_OUT,&DF1_SP,DF1_KP,DF1_KD,DF1_KI);
+pidControl DF1(&DF1_ENCREAD,&DF1_OUT,&DF1_SP,DF1_KP,DF1_KD,DF1_KI);
 pidControl DF2(&DF2_ENCREAD,&DF2_OUT,&DF2_SP,DF2_KP,DF2_KD,DF2_KI);
-pidControl DF2(&DF3_ENCREAD,&DF3_OUT,&DF3_SP,DF3_KP,DF3_KD,DF3_KI);
+pidControl DF3(&DF3_ENCREAD,&DF3_OUT,&DF3_SP,DF3_KP,DF3_KD,DF3_KI);
 
 
 void setup(){
 	Serial.begin(115200);
+  	Serial2.begin(9600);
+  
+  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc. 
+#ifdef FRONT  
+  	Front.begin(details(frontData), &Serial2); //comunicación frontal
+#endif
+#ifdef REAR
+  	Rear.begin(details(rearData), &Serial2); //comunicación trasera
+#endif
 
 	IF1.softwareLimits(250,3750);
-	IF1.controllerBegin(IF1_ENC,PB8,PB12,PB13,-65535,65535,15000);
+	IF1.controllerBegin(IF1_ENC,IF1_PWM,IF1_FWD,IF1_REV,-65535,65535,15000);
 
 	IF2.softwareLimits(250,3750);
-	IF2.controllerBegin(PA0,PB8,PB12,PB13,-65535,65535,15000);
+	IF2.controllerBegin(IF2_ENC,IF2_PWM,IF2_FWD,IF2_REV,-65535,65535,15000);
 
 	IF3.softwareLimits(250,3750);
-	IF3.controllerBegin(PA0,PB8,PB12,PB13,-65535,65535,15000);
+	IF3.controllerBegin(IF3_ENC,IF3_PWM,IF3_FWD,IF3_REV,-65535,65535,15000);
 
 	DF1.softwareLimits(250,3750);
-	DF1.controllerBegin(PA0,PB8,PB12,PB13,-65535,65535,15000);
+	DF1.controllerBegin(DF1_ENC,DF1_PWM,DF1_FWD,DF1_REV,-65535,65535,15000);
 
 	DF2.softwareLimits(250,3750);
-	DF2.controllerBegin(PA0,PB8,PB12,PB13,-65535,65535,15000);
+	DF2.controllerBegin(DF2_ENC,DF2_PWM,DF2_FWD,DF2_REV,-65535,65535,15000);
 
 	DF3.softwareLimits(250,3750);
-	DF3.controllerBegin(PA0,PB8,PB12,PB13,-65535,65535,15000);
+	DF3.controllerBegin(DF3_ENC,DF3_PWM,DF3_FWD,DF3_REV,-65535,65535,15000);
 
 }
 
 void loop(){
-	motor1.goTo(15);
-	motor1.run();
+	IF1.goTo(15);
+	IF1.run();
+
+#ifdef FRONT
+	//send the data
+	Front.receiveData();
+	 
+	IF1_SP = frontData.SSP_IF1;
+	IF2_SP = frontData.SSP_IF2;
+	IF3_SP = frontData.SSP_IF3;
+	DF1_SP = frontData.SSP_DF1;
+	DF2_SP = frontData.SSP_DF2;
+	DF3_SP = frontData.SSP_DF3;
+	    
+	/*Serial.println("SP_IF1: " + String(SP_IF1)+ " SP_IF2: " + String(SP_IF2)+ " SP_IF3: " + String(SP_IF3) + " SP_DF1: " + String(SP_DF1)+ " SP_DF2: " + String(SP_DF2)+ " SP_DF3: " + String(SP_DF3));*/   
+#endif
+
+#ifdef REAR
+	Rear.receiveData();
+
+	IR1_SP = rearData.SSP_IR1;
+	IR2_SP = rearData.SSP_IR2;
+	IR3_SP = rearData.SSP_IR3;
+	DR1_SP = rearData.SSP_DR1;
+	DR2_SP = rearData.SSP_DR2;
+	DR3_SP = rearData.SSP_DR3;
+
+	/*Serial.println("SP_IR1: " + String(SP_IR1)+ " SP_IR2: " + String(SP_IR2)+ " SP_IR3: " + String(SP_IR3) + " SP_DR1: " + String(SP_DR1)+ " SP_DR2: " + String(SP_DR2)+ " SP_DR3: " + String(SP_DR3));*/ 
+#endif
 }
